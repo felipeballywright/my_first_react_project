36434c6d9166eee64fa672950fdbbd709ca57282
felipeballywright <felipeballywright@gmail.com>
2025-06-17 19:55:20 -0300

    New exercises

--- src/Greeting.jsx
+++ src/Greeting.jsx
@@ -1,23 +1,11 @@
-function List(props) {
-  return (
-    <ul>
-      {props.animalList.map((animalName) => {
-        return animalName.startsWith("L") ? <li key={animalName}>{animalName}</li> : null;
-      })}
-    </ul>
-  );
-}
+// 8. Refactor with Keys
+// Task: Use a list of objects with unique IDs:
+// const animals = [{ name: "Lion", id: "a1" }, { name: "Tiger", id: "a2" }]
+// ✅ Render them in a list using their id as key.
+// 🔁 Then change the list to use index as a key and try reordering to see what happens.
 
-function renderAnimals() {
-  const zooAnimals = ["Lion", "Cow", "Snake", "Lizard"];
+// 9. Clickable Cards
+// Task: Create a Card component that accepts a title and an onClick handler.
+// ✅ When clicked, it should alert the title.
+// ✅ Pass the function from the parent (App), using a parameter.
 
-  return (
-    <div>
-      <h1>Animals:</h1>
-      <List animalList={zooAnimals} />
-    </div>
-  );
-}
-
-
-export default renderAnimals;
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import renderAnimals from "./Greeting.jsx";
+import RenderButtons from "./Greeting.jsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <renderAnimals />
+    <RenderButtons />
   </StrictMode>,
 );
